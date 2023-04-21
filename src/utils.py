93f1fc4685f4f92db34728a1ba9ce2ef1d4463a9
Joan Claverol <joanclaverol@Joans-MacBook-Pro.local>
2023-04-21 15:32:59 +0200

    uploaded utils

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import io
 
 def validate_csv_file(file):
     try:
         file.seek(0)  # Reset file pointer to the beginning
-        df = pd.read_csv(file)
+        df = pd.read_csv(io.StringIO(file.read().decode('utf-8')))
 
         required_columns = ['Id', 'Expensive']
         if set(required_columns).issubset(df.columns):
